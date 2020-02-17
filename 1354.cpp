8a7b3488f7b63db0040b8a16f2e0fa8130192444
kouei <kagigokoro@gmail.com>
2020-02-18 03:58:35 +0800

    add 1354

---
+++ 1354.cpp
@@ -0,0 +1,59 @@
+/****************************************************
+ * Idea:
+ * Instead of transforming [1,1,...,1] to target,
+ * try transforming target back to [1,1,...,1].
+ * Always pick up the biggest value, and subtract 
+ * the sum of other values from it.
+ * 
+ * Time:  
+ * O(N) for build the heap
+ * O(log(max value in target)log(N)) for the while loop
+ * 
+ * Space:
+ * O(N) for the heap
+ * 
+ * Supplementary Test Case:
+ * [1,1,2]
+ * [1, 1000000000]
+ * [5, 50]
+ *****************************************************/
+
+static auto io_accelerator = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
+class Solution {
+public:
+    bool isPossible(vector<int>& target) {
+        int n = target.size();
+        int64_t total = 0;
+        
+        priority_queue<int> heap;
+        for (auto i : target) {
+            total += i;
+            heap.push(i);
+        }
+        
+        while (true) {
+            auto a = heap.top();
+            
+            heap.pop();
+            total -= a;
+            
+            if (a == 1 || total == 1) return true;
+            
+            if (a < total || a % total == 0) return false;
+            
+            a %= total;
+
+            total += a;
+            heap.push(a);
+        }
+        
+        assert(false); // cannot reach here
+        return true;
+    }
+};
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# leetcode
-My LeetCode Solutions
+# My LeetCode Solutions  
+![cover](cover.png)
+
+| # | Title | Difficulty | Solution | Time | Space |
+|---| ----- | ---------- | -------- | ---- | ----- |
+|1354|[Construct Target Array With Multiple Sums](https://leetcode.com/problems/construct-target-array-with-multiple-sums/) |Hard|[C++](1354.cpp)|O(N) + O(log(max value in target)log(N))|O(N)|
